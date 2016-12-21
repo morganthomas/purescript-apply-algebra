722167078293685cba0329c1851f3867b394b2bd
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2016-12-21 15:47:59 -0700

    Add copyright boilerplate.

--- src/ApplyAlgebra.purs
+++ src/ApplyAlgebra.purs
@@ -1,3 +1,17 @@
+-- Copyright 2016 Morgan Thomas
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--    http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 -- | This module lets you generate new algebras (semigroups, monoids, etc.) by applying
 -- | applicative functors to existing algebras. An example use case: given a ring r,
 -- | fixed-length vectors of r will again form a ring when the operations are defined
